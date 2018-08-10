df5385bbe145af725540dca074217d6718154fba
Sheldon <indoing@gmail.com>
2018-08-10 11:20:02 +0800

    新增heroDetail组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; /* 这个是数据双向绑定模块 */
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component'; /*每当用命令行创建组件时，angular会自动在这个文件导入组件*/
+import { HeroesComponent } from './heroes/heroes.component';    /*每当用命令行创建组件时，angular会自动在这个文件导入组件*/
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
