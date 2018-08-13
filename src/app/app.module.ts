a783ec4f3b955a8984fc6c41abc85f4018fe338f
Sheldon <indoing@gmail.com>
2018-08-13 09:01:02 +0800

    .....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 /* 这个是数据双向绑定模块 */
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 /*每当用命令行创建组件时，angular会自动在这个文件导入组件*/
 import { HeroesComponent } from './heroes/heroes.component';
@@ -9,13 +12,15 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
